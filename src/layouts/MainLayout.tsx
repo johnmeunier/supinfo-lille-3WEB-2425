af6891797cf4284c5b62a3d83c1e20bbf57f5a25
Johnathan MEUNIER <johnathan2693@gmail.com>
2025-05-20 13:33:57 +0200

    init css

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,16 +1,19 @@
 import { Outlet } from "react-router"
+import defaultClass from "./MainLayout.module.css";
 
 function MainLayout() {
   return (
-    <>
-    <header>
-      <h1>Pokedex</h1>
-    </header>
-      <Outlet></Outlet>
-    <footer>
-      <p>Supinfo</p>
-    </footer>
-    </>
+    <div className={defaultClass.layout}>
+      <header className={defaultClass.header}>
+        <h1 className={defaultClass.title}>Pokedex</h1>
+      </header>
+      <main className={defaultClass.container}>
+        <Outlet></Outlet>
+      </main>
+      <footer className={defaultClass.footer}>
+        <p>Supinfo</p>
+      </footer>
+    </div>
   )
 }
 
