b5a3b6f91caaae8b0caec2d3af6e581af1cfecfd
Johnathan MEUNIER <johnathan2693@gmail.com>
2025-05-24 14:21:30 +0200

    change limit and actions

--- src/components/PokemonList/PokemonList.tsx
+++ src/components/PokemonList/PokemonList.tsx
@@ -1,8 +1,10 @@
+import { useState } from 'react';
 import usePokemons from './usePokemons';
 import defaultClass from './pokemonList.module.css';
 
 export default function PokemonList() {
-  const { isLoading, pokemons } = usePokemons();
+  const [limit, setLimit] = useState(20);
+  const { isLoading, pokemons, previous, goTo, next, changeLimit } = usePokemons();
 
   if (isLoading) {
     return <div>Chargement...</div>;
@@ -10,22 +12,30 @@ export default function PokemonList() {
 
   return (
     <div className={defaultClass.container}>
-      {pokemons.map((pokemon: { name: string }, index: number) => {
-        const id = index + 1;
-        return (
-          <div
-            key={pokemon.name}
-            className={defaultClass.card}
-          >
-            <h3>{pokemon.name}</h3>
-            <img
-              src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`}
-              alt={pokemon.name}
-              className={defaultClass.image}
-            />
-          </div>
-        );
-      })}
-    </div>
+      <select name="limit" id="limit" value={limit} onChange={e => {
+        setLimit(Number(e.target.value))
+        changeLimit(Number(e.target.value))
+      }}
+      >
+        <option value="10">10</option>
+        <option value="20">20</option>
+        <option value="50">50</option>
+        <option value="0">all</option>
+      </select>
+      {
+        pokemons?.map((pokemon: { name: string }) => {
+          return (
+            <div
+              key={pokemon.name}
+              className={defaultClass.card}
+            >
+              <h3>{pokemon.name}</h3>
+            </div>
+          );
+        })
+      }
+      {previous && <button onClick={() => goTo(previous)}>Previous</button>}
+      {next && <button onClick={() => goTo(next)}>Next</button>}
+    </div >
   );
 }
--- src/components/PokemonList/usePokemons.ts
+++ src/components/PokemonList/usePokemons.ts
@@ -1,18 +1,56 @@
+import { useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 
+// Get QueryClient from the context
+interface Pokemon {
+  name: string;
+  url: string;
+}
+
+interface PokemonResponse {
+  count: number;
+  results: Pokemon[];
+  previous: string | null;
+  next: string | null;
+}
+
+const baseUrl = "https://pokeapi.co/api/v2/";
+
 const usePokemons = () => {
-  const { data, isLoading, isError } = useQuery({
-    queryKey: ['pokemons'],
+  const [urlToFetch, setUrlToFetch] = useState(`${baseUrl}pokemon`);
+
+  const { data, isLoading, isError } = useQuery<PokemonResponse>({
+    queryKey: ['pokemons', urlToFetch],
     queryFn: async () => {
-      const res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=20');
-      return res.json();
+      const res = await fetch(urlToFetch);
+      if (!res.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return await res.json();
     }
   });
+
+  const changeLimit = (value: number) => {
+    if (value == 0) {
+      value = data!.count;
+    }
+    const url = new URL(urlToFetch);
+    url.searchParams.append("limit", value.toString());
+    setUrlToFetch(url.href);
+  }
+
+  const goTo = (url: string) => {
+    setUrlToFetch(url);
+  };
 
   return {
     isLoading,
     isError,
-    pokemons: data?.results || null
+    pokemons: data?.results || null,
+    previous: data?.previous || null,
+    next: data?.next || null,
+    goTo,
+    changeLimit
   };
 }
 
