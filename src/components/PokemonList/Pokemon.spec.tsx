a965a63b27d2c0400f68d54c2692c4dcc12e2079
Johnathan MEUNIER <johnathan2693@gmail.com>
2025-05-26 14:09:12 +0200

    test 20 pkms

--- src/components/PokemonList/Pokemon.spec.tsx
+++ src/components/PokemonList/Pokemon.spec.tsx
@@ -1,15 +1,25 @@
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect, beforeEach } from 'vitest';
 import { render, screen, waitFor } from '@testing-library/react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { MemoryRouter } from 'react-router';
 const queryClient = new QueryClient();
 import PokemonList from './PokemonList'
 
 describe('PokemonList', () => {
-  it('renders the PokemonList component', async () => {
-    render(<QueryClientProvider client={queryClient}>
-      <PokemonList /></QueryClientProvider>)
+  beforeEach(() => {
+    render(<QueryClientProvider client={queryClient}><MemoryRouter>
+      <PokemonList /></MemoryRouter></QueryClientProvider>)
+  });
+
+  it('renders 20 pokemons', async () => {
     await waitFor(() => {
-      expect(screen.getByText("charizard")).toBeTruthy();
+      expect(screen.getAllByRole("heading", { level: 3 })).length(20);
     })
-  })
+  });
+
+  it('renders charizard', async () => {
+    await waitFor(() => {
+      expect(screen.getByRole('heading', { level: 3, name: 'charizard' }))
+    })
+  });
 })
