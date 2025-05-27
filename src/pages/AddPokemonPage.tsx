376a7b26eadea2603dd6fac50acdc57a845fbd13
Clément Guilbaud <115244309+DonThatch@users.noreply.github.com>
2025-05-27 14:08:16 +0200

    ajout de form pokemon

--- src/pages/AddPokemonPage.tsx
+++ src/pages/AddPokemonPage.tsx
@@ -1,3 +1,34 @@
 export const AddPokemonPage = () => {
-    return <h1>Add Pokemon</h1>
+
+    return (
+    <>
+        <h1>Add Pokemon</h1>
+        <form onSubmit={(e) => {
+            e.preventDefault();
+            const form = e.currentTarget;
+            const formElement = form.elements as typeof form.elements & {
+
+                name: {value: string}
+
+            }
+            console.log(formElement.name.value);
+        }}>
+            <label>
+                Name:
+                <input type="text" name="name"/>
+            </label>
+            <br/>
+            <label>
+                Type:
+                <input type="text" name="type"/>
+            </label>
+            <br/>
+            <label>
+                Image URL:
+                <input type="url" name="image"/>
+            </label>
+            <button type="submit">Add Pokemon</button>
+        </form>
+    </>
+    )
 };
